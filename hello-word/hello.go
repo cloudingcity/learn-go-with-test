509b0c13d8ccc4c21a202ab3b001fc4922478cce
clouding <ghost0436@gmail.com>
2019-09-02 00:44:01 +0800

    Add hello-world

---
+++ hello-word/hello.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, lang string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(lang) + name
+}
+
+func greetingPrefix(lang string) (prefix string) {
+	switch lang {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
